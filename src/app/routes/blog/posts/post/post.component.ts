392e2b7d283e422802e6df862b321c6d298f0c86
ewinchen <chenyongp@esquel.com>
2018-05-19 11:32:14 +0800

    feat: use rxjs to produce post

--- src/app/routes/blog/posts/post/post.component.ts
+++ src/app/routes/blog/posts/post/post.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Post, PostStructure } from './post';
 import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnDestroy {
 
   post: PostStructure;
+  fetchPost$: Subscription;
 
   constructor(private router: Router, private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit() {
-    // this.post = new Post(
-    //   'Angular rolling',
-    //   'Just try your best, but be careful, you must keep yourself be healthy',
-    //   ['IT', 'Angular', 'Web'],
-    //   new Date(),
-    //   new Date()
-    // );
-    this.postService.fetchPost().subscribe(post => this.post = post);
+    this.fetchPost$ = this.postService.fetchPost().subscribe(post => this.post = post);
+  }
+
+  ngOnDestroy() {
+    this.fetchPost$.unsubscribe();
   }
 
 }
--- src/app/routes/blog/posts/post/post.service.ts
+++ src/app/routes/blog/posts/post/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PostStructure, Post } from './post';
 import { Observable, from, of, interval } from 'rxjs';
-import { timeInterval } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 
 import { HttpClient } from '@angular/common/http';
@@ -11,42 +11,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PostService {
 
-  post: Array<PostStructure>;
 
   constructor(private http: HttpClient) {
-    this.post = [
-      new Post(
-        'Angular rolling 1',
-        'Just try your best, but be careful, you must keep yourself be healthy',
-        ['IT', 'Angular', 'Web'],
-        new Date(),
-        new Date()
-      ),
-      new Post(
-        'Angular rolling 2',
-        'Just try your best, but be careful, you must keep yourself be healthy',
-        ['IT', 'Angular', 'Web'],
-        new Date(),
-        new Date()
-      ),
-      new Post(
-        'Angular rolling 3',
-        'Just try your best, but be careful, you must keep yourself be healthy',
-        ['IT', 'Angular', 'Web'],
-        new Date(),
-        new Date()
-      ),
-      new Post(
-        'Angular rolling 4',
-        'Just try your best, but be careful, you must keep yourself be healthy',
-        ['IT', 'Angular', 'Web'],
-        new Date(),
-        new Date()
-      ),
-    ];
   }
 
   fetchPost(): Observable<PostStructure> {
-    return from(this.post);
+    return interval(1000).pipe(
+      map(() =>  new Post(
+        'Angular rolling' + Math.random(),
+        'Just try your best, but be careful, you must keep yourself be healthy',
+        ['IT', 'Angular', 'Web'],
+        new Date(),
+        new Date()
+      ))
+    );
   }
 }
